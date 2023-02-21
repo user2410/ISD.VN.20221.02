6c2546cfc72e6ea5ecefc37961646d475cd70fb3
user2410 <dc241024@gmail.com>
2023-02-21 16:30:02 +0700

    fix payment info test

--- source/src/test/java/ValidatePaymentInfoTest.java
+++ source/src/test/java/ValidatePaymentInfoTest.java
@@ -1,7 +1,7 @@
 import edu.hust.vn.common.exception.invalid_payment_info.InvalidPaymentInfoException;
-import edu.hust.vn.controller.strategy.paymentinfo_validation.CardValidationStrategy;
-import edu.hust.vn.controller.strategy.paymentinfo_validation.PaymentInfoValidationStrategy;
-import org.junit.jupiter.api.BeforeAll;
+import edu.hust.vn.model.payment.PaymentCreditCard;
+import edu.hust.vn.model.payment.PaymentMethod;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,12 +10,12 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidatePaymentInfoTest {
-    PaymentInfoValidationStrategy strategy;
+    PaymentMethod paymentMethod;
     HashMap<String, String> info;
 
-    @BeforeAll
+    @BeforeEach
     void setUp(){
-        strategy = new CardValidationStrategy();
+        paymentMethod = new PaymentCreditCard();
         info = new HashMap<>();
     }
 
@@ -29,12 +29,12 @@ public class ValidatePaymentInfoTest {
         "987125_group2_2023, Abc Def, 12/02/2023, 1234, true",
     })
     public void test(String cardCode, String cardOwner, String expDate, String cardCvv, boolean successIfException){
-        info.put("cardNumber", cardCode);
+        info.put("cardCode", cardCode);
         info.put("cardOwner", cardOwner);
         info.put("expDate", expDate);
         info.put("cvvCode", cardCvv);
         try{
-            strategy.validate(info);
+            paymentMethod.createPaymentEntity(info);
             assertFalse(successIfException);
         }catch (InvalidPaymentInfoException e){
             assertTrue(successIfException);
